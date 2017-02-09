8a281b900079a8b649a806461d1ae295963aaf50
Marc Larkin and Sam Kirsch <marc+sam@>
2017-02-09 08:44:14 -0800

    initialize the base files to link with jquery, bootstrap, nancy, and razor, along with initializing the project.lock file

---
+++ HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using System.Collections.Generic;
+using HangmanGame.Objects;
+
+namespace HangmanGame
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            // PUT ROUTES HERE
+        }
+    }
+}
